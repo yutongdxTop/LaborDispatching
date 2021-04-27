824b9aa739c16bd426cc3401a6ffdd6bbd8391c7
yuTong <unicon615@gmail.com>
2021-04-27 17:05:18 +0800

    init project

---
+++ src/main/java/com/yutongdxTop/LaborDispatching/LaborDispatchingApplication.java
@@ -0,0 +1,13 @@
+package com.yutongdxTop.LaborDispatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborDispatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborDispatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yutongdxTop/LaborDispatching/LaborDispatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yutongdxTop.LaborDispatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborDispatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
